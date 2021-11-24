3f2f3fb7478627fdcac1067efe3f93f8a6e15ba6
Daniel Kamel <danielgohara@gmail.com>
2021-11-23 20:25:37 -0500

    Update home.php
    shows current user in cornner

--- home.php
+++ home.php
@@ -1,3 +1,27 @@
-<?php echo "hi" ;
+<?php //echo "hi" ;
+// Include and call function to connect to db
+include_once 'components/dbConnection.php';
+$conn = getConnection();
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+session_start();
+
+if ($_SESSION['sessionMode'] == "student"){
+    $sql = "SELECT * FROM student WHERE studentNo = ". $_SESSION["sessionID"].";";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    echo "<table style='border-style: solid'>";
+    echo "<tr>";
+    echo "<th>Num</th><th>Name</th>";
+    echo "</tr>";
+    echo "<tr>";
+    echo "<td>".$row['studentNo']."</td><td>".$row['fName']." ".$row['lName']."</td>";
+    echo "</tr>";
+    echo "</table>";
+}
+
 
 ?>
